f6360a263d50a63a90c109f546eb8c51db4e4295
Paola Andrea Vallejo Correa <pvallej3@eafit.edu.co>
2025-07-29 09:36:53 -0500

    Primer commit

---
+++ movie/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    # código HTML en views :(
+    # return HttpResponse('<h1>Welcome to Home Page</h1>')
+    
+    #uso de plantilla sin parámetros
+    #return render(request, 'home.html')
+
+    # uso de plantilla con parámetros
+    return render(request, 'home.html', {'name':'Greg Lim'})
+
+ # Función para 'About'
+def about(request):
+    return HttpResponse('<h1>Welcome to About Page</h1>')
